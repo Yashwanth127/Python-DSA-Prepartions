c909d2eed19199e98bb9f0cdb88a0e8237e877c9
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-03-30 22:03:30 +0530

    olved 3 Python problems: count 1s in list, logical operators, multiples of 2 or 3

---
+++ array/(modules)how many numbers will be divided by ___ number .py
@@ -0,0 +1,6 @@
+num=[2,23,4,55,1,32,2,223,2,2,2,23445,56]
+ans=0
+for i in num:
+    if i % 4==0:
+        ans=ans+1
+print(ans)
---
+++ array/and or operator.py
@@ -0,0 +1,7 @@
+#if (1+2==3 or 2+3==5): Your if (1+2==3 and 2+3==5): code prints yes, since both conditions are true
+if (1+2==3 and 2+3==5):
+    print("yes")
+else:
+    print("wrong")
+
+
