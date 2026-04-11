d735dc6297d0b68b48dc64b8bca026685cbcf0c8
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-04-11 08:37:52 +0530

    Solved Two Sum, Stock Profit, Move Zeroes, and Missing Number

---
+++ move Zeros.py
@@ -0,0 +1,12 @@
+def move_zeroes(nums):
+    index = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[index], nums[i] = nums[i], nums[index]
+            index += 1
+
+    return nums
+
+nums = [0, 1, 0, 3, 12]
+print(move_zeroes(nums))
